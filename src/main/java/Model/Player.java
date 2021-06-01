97ceb365f59f44afc2978837fe2c0eab19da935f
TVG98 <s1128688@student.hsleiden.nl>
2021-06-01 14:06:10 +0200

    Created basic Player class

---
+++ src/main/java/Model/Player.java
@@ -0,0 +1,18 @@
+package Model;
+
+import java.util.ArrayList;
+
+public class Player {
+    //private ArrayList<PlayerCard> hand = new ArrayList<PlayerCard>();
+    //private Role role;
+    //private City currentCity;
+    private boolean readyToStart = false;
+    private String name;
+    private int actions;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    
+}
