12d47620f861794c206e4bd761c9a5e3a43aafad
wimb64 <wimb64@icloud.com>
2021-06-01 15:12:59 +0200

    added model.cube

---
+++ src/main/java/Model/Cube.java
@@ -0,0 +1,13 @@
+package Model;
+
+public class Cube {
+    private String type;
+
+    public Cube(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
