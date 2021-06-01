119ce95944cf51eac42123c225693c58362c5828
wimb64 <wimb64@icloud.com>
2021-06-01 15:11:11 +0200

    added model.city

---
+++ src/main/java/Model/City.java
@@ -0,0 +1,27 @@
+package Model;
+
+import java.util.ArrayList;
+
+public class City {
+    private String name;
+    private ArrayList<Cube> cubes;
+    private String virusType;
+    private ArrayList<City> nearCities;
+    private boolean hasStation;
+
+    public City(String name, String virusType, ArrayList<City> nearCities, boolean hasStation) {
+        this.name = name;
+        this.virusType = virusType;
+        this.nearCities = nearCities;
+        this.hasStation = hasStation;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCubeAmount() {
+        return this.cubes.size();
+    }
+
+}
