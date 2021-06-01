f555f60fe101cb4b1966c92b2df73538018ee0c8
TVG98 <s1128688@student.hsleiden.nl>
2021-06-01 14:26:49 +0200

    Added functionality to player class
    commented out code will give errors if no City, Role and PlayerCard classes are implemented

--- src/main/java/Model/Player.java
+++ src/main/java/Model/Player.java
@@ -3,16 +3,75 @@ package Model;
 import java.util.ArrayList;
 
 public class Player {
-    //private ArrayList<PlayerCard> hand = new ArrayList<PlayerCard>();
+    //private ArrayList<PlayerCard> hand;
     //private Role role;
     //private City currentCity;
-    private boolean readyToStart = false;
+    private boolean readyToStart;
     private String name;
     private int actions;
 
-    public Player(String name) {
+    /*public Player(String name, City city, Role role) {
         this.name = name;
+        actions = 4;
+        readyToStart = false;
+        currentCity = city;
+        this.role = role;
+        hand = new ArrayList<PlayerCard>();
+    }*/
+
+    public void endTurn() {
+        resetActions();
     }
 
-    
+    /*public void addCardToHand(PlayerCard card) {
+        hand.add(card);
+    }*/
+
+    /*public void removeCardFromHand(PlayerCard card) {
+        for (PlayerCard playerCard : hand) {
+            if (playerCard = card) {
+                hand.remove(playerCard);
+            }
+        }
+    }*/
+
+    /*public ArrayList<PlayerCard> getHand() {
+        return hand;
+    }*/
+
+    /*public Role getRole() {
+        return role;
+    } */
+
+    /*public void setRole(Role role) {
+        this.role = role;
+    } */
+
+    /*public City getCurrentCity() {
+        return currentCity;
+    } */
+
+    /*public setCurrentCity(City city) {
+        this.city = city;
+    } */
+
+    public boolean getReadyToStart() {
+        return readyToStart;
+    }
+
+    public void setReadyToStart() {
+        readyToStart = true;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void resetActions() {
+        actions = 4;
+    }
+
+    private void decrementActions() {
+        actions--;
+    }
 }
