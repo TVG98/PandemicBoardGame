97e8a26bcb4d72810f5fac2475c60f703ad88493
TVG98 <s1128688@student.hsleiden.nl>
2021-06-16 15:12:43 +0200

    Updated playerObservable interface so observer can access data in playerObservable

--- src/main/java/Observers/PlayerObservable.java
+++ src/main/java/Observers/PlayerObservable.java
@@ -1,6 +1,12 @@
 package Observers;
 
+import Model.City;
+
 public interface PlayerObservable {
     void register(PlayerObserver observer);
     void notifyAllObservers();
+    String getRoleAsString();
+    City getCurrentCity();
+    String getPlayerName();
+    int getActions();
 }
