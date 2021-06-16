8e115ed9e374c7c6cd1caa112c10f9dbb6e6b61e
thimo <thimoquinten@gmail.com>
2021-06-16 11:44:05 +0200

    added functionality to make EdgeConnectionsToCities

--- src/main/java/Model/Connection.java
+++ src/main/java/Model/Connection.java
@@ -12,6 +12,34 @@ public class Connection {
         this.cityTwo = cityTwo;
     }
 
+    public int getShortestX() {
+        return Math.min(cityOne[0], cityTwo[0]);
+    }
+
+    public int getShortestY() {
+        return Math.min(cityOne[1], cityTwo[1]);
+    }
+
+    public int getYDifference() {
+        return Math.abs(cityOne[1] - cityTwo[1]);
+    }
+
+    public int[] getCityOne() {
+        return cityOne;
+    }
+
+    public int[] getCityTwo() {
+        return cityTwo;
+    }
+
+    public int[] getLeftCity() {
+        return (cityOne[0] < cityTwo[0]) ? cityOne : cityTwo;
+    }
+
+    public int[] getRightCity() {
+        return (cityOne[0] > cityTwo[0]) ? cityOne : cityTwo;
+    }
+
     public Line getLineFromConnection() {
         Line line = new Line();
         line.setStartX(cityOne[0]);
